1dea3ee71f10e51e245a85e81f8e1ee7d5bfdc75
Frank Torres <7954988+fetorres@users.noreply.github.com>
2018-09-07 15:22:37 -0700

    Update set_environ.sh

--- set_environ.sh
+++ set_environ.sh
@@ -1,1 +1,8 @@
-
+#!/usr/bin/env bash
+export TSN_ROOT='/data/user/temporal-segment-networks'
+export TSN_ENVIRON='TSN'
+export API_CLIENT_USERNAME='your_username'
+export API_CLIENT_PASSWORD='your_password'
+export BROKER_THREADING='True'
+export COMPUTE_EPS=.000003
+export RANDOM_SEED=73459912436
